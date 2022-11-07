c420a91110aab1963e7eb0ef50410ea217d5fa35
Dylan <dylan.p.maus@gmail.com>
2022-11-06 22:51:31 -0500

    updates

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -6,13 +6,9 @@ apt install openssh-server -y
 apt install lm-sensors -y
 apt install curl -y
 apt install vim -y
-apt install dropbear-initramfs -y
-apt install xrdp -y
 apt install git -y
 apt install python3.10-venv -y
 apt install python3-pip -y
 apt autoremove -y
-systemctl enable xrdp
 systemctl enable ssh
-ufw allow 3389/tcp
 ufw allow ssh
